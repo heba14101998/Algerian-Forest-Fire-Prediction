58a6b1ec18df8b5f00e13c277d7c0aff7239eecf
Heba Mohamed <mohamedheba1998@gmail.com>
2024-06-13 16:04:10 +0300

    ubuntu python 3.9

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,6 +3,7 @@ import sys
 import shutil
 import pandas as pd
 from dotenv import load_dotenv
+from kaggle.api.kaggle_api_extended import KaggleApi
 
 from src.exception import CustomException
 from src.logger import logging
@@ -47,6 +48,34 @@ class DataIngestor:
 
         logging.info(f"Dataset '{DATASET_API}' downloaded to '{self.configs.raw_data_dir}'")
 
+
+    # def download_dataset(self):
+    #     os.makedirs(self.configs.raw_data_dir, exist_ok=True)  # Create the output directory if it doesn't exist
+    #     # Get owner_slug
+    #     owner_slug = DATASET_API.split('/')[0]
+    #     # Get the dataset name
+    #     dataset_name = DATASET_API.split('/')[1]
+
+    #     try:
+    #         # Authenticate to Kaggle API
+    #         api = KaggleApi()
+    #         api.authenticate()
+
+    #         # Download the dataset
+    #         api.datasets_download(owner_slug, dataset_name)
+    #         logging.info(f"Dataset '{dataset_name}' downloaded to '{self.configs.raw_data_dir}'")
+
+    #         # Extract the downloaded ZIP file (if needed)
+    #         for filename in os.listdir(self.configs.raw_data_dir):
+    #             if filename.endswith(".zip"):
+    #                 zip_path = os.path.join(self.configs.raw_data_dir, filename)
+    #                 shutil.unpack_archive(zip_path, self.configs.raw_data_dir, 'zip')
+    #                 os.remove(zip_path) 
+
+    #         logging.info(f"Unpack '{dataset_name}' to '{self.configs.raw_data_dir}'")
+
+    #     except Exception as e:
+    #         raise CustomException(e, sys)
 
 if __name__ == '__main__':
 
