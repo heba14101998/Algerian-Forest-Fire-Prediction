cc7773409e993a1521e77ca047c7bd78b7325f1a
Heba Mohamed <mohamedheba1998@gmail.com>
2024-06-13 19:19:34 +0300

    ubuntu 0.0.1

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,9 +4,10 @@ import shutil
 import pandas as pd
 from dotenv import load_dotenv
 
-from src.exception import CustomException
-from src.logger import logging
-from src.utils import read_yaml
+from .utils import read_yaml
+from .exception import CustomException
+from .logger import logging
+from .utils import read_yaml
 
 # Load environment variables
 load_dotenv()
