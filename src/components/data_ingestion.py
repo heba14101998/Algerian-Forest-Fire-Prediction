38fc0299ad4c5e2be5879e1f6dc646930fec701d
Heba Mohamed <mohamedheba1998@gmail.com>
2024-06-13 19:02:52 +0300

    ubuntu 0.0.1

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -49,34 +49,6 @@ class DataIngestor:
         logging.info(f"Dataset '{DATASET_API}' downloaded to '{self.configs.raw_data_dir}'")
 
 
-    # def download_dataset(self):
-    #     os.makedirs(self.configs.raw_data_dir, exist_ok=True)  # Create the output directory if it doesn't exist
-    #     # Get owner_slug
-    #     owner_slug = DATASET_API.split('/')[0]
-    #     # Get the dataset name
-    #     dataset_name = DATASET_API.split('/')[1]
-
-    #     try:
-    #         # Authenticate to Kaggle API
-    #         api = KaggleApi()
-    #         api.authenticate()
-
-    #         # Download the dataset
-    #         api.datasets_download(owner_slug, dataset_name)
-    #         logging.info(f"Dataset '{dataset_name}' downloaded to '{self.configs.raw_data_dir}'")
-
-    #         # Extract the downloaded ZIP file (if needed)
-    #         for filename in os.listdir(self.configs.raw_data_dir):
-    #             if filename.endswith(".zip"):
-    #                 zip_path = os.path.join(self.configs.raw_data_dir, filename)
-    #                 shutil.unpack_archive(zip_path, self.configs.raw_data_dir, 'zip')
-    #                 os.remove(zip_path) 
-
-    #         logging.info(f"Unpack '{dataset_name}' to '{self.configs.raw_data_dir}'")
-
-    #     except Exception as e:
-    #         raise CustomException(e, sys)
-
 if __name__ == '__main__':
 
     configs, _ = read_yaml('params.yaml')
